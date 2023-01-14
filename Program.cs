b09434d50e5026f5e60a17569c1e3417e690113f
spatialfree <mail@ethanmerchant.com>
2023-01-14 05:38:44 -0500

    Zen

--- Program.cs
+++ Program.cs
@@ -1,33 +1,26 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using ai.Data;
+global using Blazored.LocalStorage;
+global using Blazored.LocalStorage.Serialization;
+global using OpenAI;
+global using OpenAI.Edits;
 
 var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddRazorPages();
+    builder.Services.AddServerSideBlazor();
+    builder.Services.AddBlazoredLocalStorage();
+    // builder.Services.Replace(ServiceDescriptor.Scoped<IJsonSerializer, NewtonSoftJsonSerializer>());
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+    // var apiKey = builder.Configuration["OpenAI:ApiKey"];
+    // builder.Services.AddSingleton<OpenAIService>(sp => new OpenAIService(apiKey)); // singleton?
 
-var apiKey = builder.Configuration["OpenAI:ApiKey"];
-builder.Services.AddSingleton<OpenAIService>(sp => new OpenAIService(apiKey));
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
+    if (!app.Environment.IsDevelopment()) { // Configure the HTTP request pipeline.
+        app.UseExceptionHandler("/Error");
+        app.UseHsts(); // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    }
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
+    app.MapBlazorHub();
+    app.MapFallbackToPage("/_Host");
 app.Run();

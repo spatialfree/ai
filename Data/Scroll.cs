557847f7889005411ad8be9eb41c6ef1cb0bfece
spatialfree <mail@ethanmerchant.com>
2023-01-15 08:42:39 -0500

    late slate

--- Data/Scroll.cs
+++ Data/Scroll.cs
@@ -1,7 +1,7 @@
 public class Scroll {
 	public string? Label { get; set; }
 	public string? Text  { get; set; }
-	public string? Full => $"{Label}:\n{Text}\n\n";
+	public string? Full => $"{Label}\n{Text}\n\n";
 	public string? Color { get; set; }
 	// public bool IsDone { get; set; }
 }

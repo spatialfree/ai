71bb29af0ee8d5c663b6fce50156d62e6925402e
spatialfree <mail@ethanmerchant.com>
2023-02-16 23:57:48 -0500

    file fission

--- Pages/IndexBase.cs
+++ Pages/IndexBase.cs
@@ -303,45 +303,11 @@ button {
 	}
 
 	Pointer[] pointers = new Pointer[2];
-	class Pointer {
-		IndexBase index;
-		public Pointer(IndexBase index) {
-			this.index = index;
-		}
-
-		public long id = -1;
-		public Vec screen = new Vec();
-		public Vec canvas { get { return screen - index.Canvas; } }
-
-		public bool dwn = false;
-		public bool dbl = false;
-		DateTime lastDown = DateTime.Now;
-		public void Down(double x, double y, long id) {
-			screen = new Vec(x, y);
-			this.id = id;
-
-			TimeSpan time = DateTime.Now - lastDown;
-			dbl = (screen - lastUp).Mag < 20 && time.TotalMilliseconds < 500;
-			lastDown = DateTime.Now;
-			dwn = true;
-		}
-
-		public void Move(double x, double y) {
-			screen = new Vec(x, y);
-		}
-
-		public Vec lastUp = new Vec();
-		public void Up() {
-			lastUp = screen;
-
-			dwn = false;
-		}
-	}
 
 	Vec offset = new Vec(0, 0);
 
 	Vec canvasOffset = new Vec(0, 0);
-	protected Vec Canvas = new Vec(0, 0);
+	public Vec Canvas = new Vec(0, 0);
 
 	protected bool drag = false;
 	protected bool held = false;
---
+++ Pointer.cs
@@ -0,0 +1,36 @@
+namespace ai;
+
+public class Pointer {
+  IndexBase index;
+  public Pointer(IndexBase index) {
+    this.index = index;
+  }
+
+  public long id = -1;
+  public Vec screen = new Vec();
+  public Vec canvas { get { return screen - index.Canvas; } }
+
+  public bool dwn = false;
+  public bool dbl = false;
+  DateTime lastDown = DateTime.Now;
+  public void Down(double x, double y, long id) {
+    screen = new Vec(x, y);
+    this.id = id;
+
+    TimeSpan time = DateTime.Now - lastDown;
+    dbl = (screen - lastUp).Mag < 20 && time.TotalMilliseconds < 500;
+    lastDown = DateTime.Now;
+    dwn = true;
+  }
+
+  public void Move(double x, double y) {
+    screen = new Vec(x, y);
+  }
+
+  public Vec lastUp = new Vec();
+  public void Up() {
+    lastUp = screen;
+
+    dwn = false;
+  }
+}
